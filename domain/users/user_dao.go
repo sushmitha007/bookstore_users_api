713d5ef607650ba3dc7889bb3d6948eac6eb4dcd
sushmitha007 <sushmitha.cs18@gmail.com>
2021-01-03 17:14:36 +0530

    updated user dao

---
+++ domain/users/user_dao.go
@@ -0,0 +1,10 @@
+package users
+
+import "github.com/sushmitha007/bookstore_users_api/util/errors"
+
+func (user *User) Save() *errors.RestErr {
+	return nil
+}
+func (user *User) Get() *errors.RestErr {
+	return nil
+}
